ae74d831c6ff81ea632712981b71f8309b8e211f
KonYaco <atwzj233@gmail.com>
2025-08-26 00:19:34 +0800

    [feat](util) Add IsBlank util

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,1 +1,17 @@
 pub mod gracefully_shutdown;
+
+pub trait IsBlank {
+    fn is_blank(&self) -> bool; 
+}
+
+impl IsBlank for str {
+    fn is_blank(&self) -> bool {
+        self.is_empty() || self.chars().all(char::is_whitespace)
+    }
+}
+
+impl IsBlank for String {
+    fn is_blank(&self) -> bool {
+        self.as_str().is_blank()
+    }
+}
