e29437742b02141cc7e93752215a28a46eaabaac
KonYaco <atwzj233@gmail.com>
2025-09-02 20:30:11 +0800

    [feat](mailer) Support HTML verification code email

--- src/service/mailer/mod.rs
+++ src/service/mailer/mod.rs
@@ -1,14 +1,21 @@
-use lettre::message::header::ContentType;
-use lettre::message::Mailbox;
+use axum::extract::Multipart;
+use lettre::message::header::{ContentTransferEncoding, ContentType};
+use lettre::message::{Mailbox, MultiPart, SinglePart};
 use lettre::{SmtpTransport, Transport};
 use lettre::transport::smtp::authentication::Credentials;
+use openssl::base64;
 use crate::web::routes::auth::EmailConfig;
 
+const EMAIL_TEMPLATE: &str = include_str!("templates/code_mail_template_zh.html");
+const EMAIL_PLAIN_TEMPLATE: &str = include_str!("templates/code_mail_template_zh.txt");
 pub async fn send_verification_code(
     cfg: &EmailConfig,
     to: &str,
-    code: &str
+    code: &str,
 ) -> anyhow::Result<()> {
+    let html_content = EMAIL_TEMPLATE.replace("{{VERIFICATION_CODE}}", code);
+    let plain_content = EMAIL_PLAIN_TEMPLATE.replace("{{VERIFICATION_CODE}}", code);
+
     let email_msg = lettre::Message::builder()
         .from(Mailbox::new(
             Some("Hachimi World".to_string()),
@@ -16,8 +23,14 @@ pub async fn send_verification_code(
         ))
         .to(Mailbox::new(None, to.parse()?))
         .subject("Your email verification code - Hachimi World")
-        .header(ContentType::TEXT_PLAIN)
-        .body(code.to_string())?;
+        .multipart(MultiPart::alternative()
+            .singlepart(SinglePart::plain(plain_content))
+            .singlepart(SinglePart::builder()
+                .header(ContentType::TEXT_HTML)
+                .header(ContentTransferEncoding::Base64)
+                .body(html_content)
+            )
+        )?;
 
     let creds = Credentials::new(cfg.username.clone(), cfg.password.clone());
 
@@ -39,7 +52,6 @@ mod test {
         let content = fs::read_to_string("config.yaml").unwrap();
         let value = serde_yaml::from_str::<serde_yaml::Value>(content.as_str()).unwrap();
         let cfg: EmailConfig = serde_yaml::from_value(value["email"].clone()).unwrap();
-        println!("{:?}", cfg);
-        send_verification_code(&cfg, "mail@example.com", "Your email verification code is: AFKC-ADI2").await.unwrap();
+        send_verification_code(&cfg, "mail@example.com", "114514").await.unwrap();
     }
 }
