85a8d2af2e39777308de50c37f5dba5328ed25c1
KonYaco <atwzj233@gmail.com>
2026-07-01 16:56:06 +0800

    [deps] Update `symphonia` to 0.6

--- src/audio/mod.rs
+++ src/audio/mod.rs
@@ -1,13 +1,15 @@
 use anyhow::{anyhow, Context};
 use replaygain::ReplayGain;
-use symphonia::core::audio::{SampleBuffer, SignalSpec};
+use symphonia::core::audio::sample::Sample;
+use symphonia::core::audio::AudioSpec;
 use symphonia::core::codecs;
-use symphonia::core::codecs::{CodecType, DecoderOptions};
+use symphonia::core::codecs::audio::{AudioCodecId, AudioDecoderOptions};
 use symphonia::core::errors::Error as SymphoniaError;
-use symphonia::core::formats::{FormatOptions, FormatReader, Track};
+use symphonia::core::formats::probe::Hint;
+use symphonia::core::formats::{FormatOptions, FormatReader, Track, TrackType};
 use symphonia::core::io::{MediaSource, MediaSourceStream, MediaSourceStreamOptions};
-use symphonia::core::meta::{MetadataOptions, StandardTagKey};
-use symphonia::core::probe::Hint;
+use symphonia::core::meta::{MetadataOptions, StandardTag};
+use symphonia::core::units::Timestamp;
 use thiserror::Error;
 use tracing::{debug, info, warn};
 
@@ -37,7 +39,7 @@ pub struct PickedMetadata {
     pub sample_rate: u32,
     pub duration_secs: u64,
     pub peak: f32,
-    pub gain_db: f32
+    pub gain_db: f32,
 }
 
 pub fn parse_and_validate(
@@ -56,9 +58,11 @@ pub fn parse_and_validate(
         gain_db: 0f32,
     };
 
+    // Init the media source stream
     let media = MediaSourceStream::new(input, MediaSourceStreamOptions::default());
-    let mut hint = Hint::default();
 
+    // Use file extension as hint if possible
+    let mut hint = Hint::new();
     if let Some((_, ext)) = file_name.and_then(|name| name.rsplit_once(".")) {
         debug!("File extension hint: {ext}");
         hint.with_extension(ext);
@@ -66,27 +70,27 @@ pub fn parse_and_validate(
         debug!("No hint");
     }
 
-    let mut probed = symphonia::default::get_probe()
-        .format(&hint, media, &FormatOptions::default(), &MetadataOptions::default())
+    // Use default format options and metadata options to probe the format
+    let meta_opts: MetadataOptions = Default::default();
+    let fmt_opts: FormatOptions = Default::default();
+
+    // Probe the media source stream for a format.
+    let mut format = symphonia::default::get_probe()
+        .probe(&hint, media, fmt_opts, meta_opts)
         .map_err(|x| ParseError::Parse(x))?;
 
-    let metadata = probed.format
-        .metadata().current()
-        .map(|x| x.to_owned())
-        .or_else(|| {
-            probed
-                .metadata
-                .get()
-                .and_then(|m| m.current().map(|r| r.to_owned()))
-        })
+    // Retrieve the first metadata found in the format\
+    let metadata = (&mut format).metadata();
+    let metadata = metadata.current()
         .ok_or_else(|| ParseError::MetadataNotFound("root".to_string()))?;
 
+
     // Retrieve metadata
-    for tag in metadata.tags() {
-        if let Some(key) = tag.std_key {
+    for tag in &metadata.media.tags {
+        if let Some(key) = tag.std.to_owned() {
             match key {
-                StandardTagKey::TrackTitle => result.title = Some(tag.value.to_string()),
-                StandardTagKey::Artist => result.artist = Some(tag.value.to_string()),
+                StandardTag::TrackTitle(value) => result.title = Some(value.to_string()),
+                StandardTag::Artist(value) => result.artist = Some(value.to_string()),
                 // StandardTagKey::Arranger => {}
                 // StandardTagKey::Bpm => {}
                 _ => {
@@ -100,17 +104,24 @@ pub fn parse_and_validate(
     // meta.visuals()
 
     // Find the default audio track
-    let track = probed.format.default_track().ok_or_else(|| ParseError::TrackNotFound)?;
+    let track = format.default_track(TrackType::Audio).ok_or_else(|| ParseError::TrackNotFound)?;
 
     info!("Track found, audio codec: {:?}", track.codec_params);
+    let codec_params = if let Some(codec) = &track.codec_params &&
+        let Some(audio_codec) = codec.audio() {
+        audio_codec
+    } else {
+        Err(ParseError::FormatUnsupported)?
+    };
+
 
     /*result.bits_per_sample =
         track.codec_params.bits_per_sample.ok_or_else(|| ParseError::MetadataNotFound("bits_per_sample".to_string()))?;*/
-    result.format = get_format_str(track.codec_params.codec).ok_or_else(|| ParseError::FormatUnsupported)?.to_string();
+    result.format = get_format_str(codec_params.codec).ok_or_else(|| ParseError::FormatUnsupported)?.to_string();
     // Calculate duration
     result.duration_secs = calculate_duration_secs(&track)?.ok_or_else(|| ParseError::ParsingDurationError)?;
-    result.sample_rate = track.codec_params.sample_rate.unwrap_or(0);
-    let (gain, peak) = calculate_gain_peak(&mut probed.format)
+    result.sample_rate = codec_params.sample_rate.unwrap_or(0);
+    let (gain, peak) = calculate_gain_peak(&mut format)
         .map_err(|x| {
             warn!("Failed to calculate gain/peak: {x:?}");
             ParseError::CalculatingGainPeakError
@@ -120,19 +131,20 @@ pub fn parse_and_validate(
     Ok(result)
 }
 
-fn get_format_str(codec_type: CodecType) -> Option<&'static str> {
+fn get_format_str(codec_type: AudioCodecId) -> Option<&'static str> {
     match codec_type {
-        codecs::CODEC_TYPE_MP3 => Some("mp3"),
-        codecs::CODEC_TYPE_AAC => Some("aac"),
-        codecs::CODEC_TYPE_FLAC => Some("flac"),
+        codecs::audio::well_known::CODEC_ID_MP3 => Some("mp3"),
+        codecs::audio::well_known::CODEC_ID_AAC => Some("aac"),
+        codecs::audio::well_known::CODEC_ID_FLAC => Some("flac"),
         _ => None
     }
 }
+
 fn calculate_duration_secs(track: &Track) -> Result<Option<u64>, ParseError> {
-    let r = if let Some(tb) = track.codec_params.time_base {
-        let frames = track.codec_params.n_frames.ok_or_else(|| ParseError::ParsingDurationError)?;
-        let duration = tb.calc_time(frames);
-        Some(duration.seconds)
+    let r = if let Some(tb) = track.time_base {
+        let frames = track.num_frames.ok_or_else(|| ParseError::ParsingDurationError)?;
+        let duration = tb.calc_time(Timestamp::new(frames as i64)).ok_or_else(|| ParseError::ParsingDurationError)?;
+        Some(duration.as_secs() as u64)
     } else {
         None
     };
@@ -141,71 +153,56 @@ fn calculate_duration_secs(track: &Track) -> Result<Option<u64>, ParseError> {
 
 fn calculate_gain_peak(format: &mut Box<dyn FormatReader>) -> anyhow::Result<(f32, f32)> {
     let (spec, samples) = read_interleaved_samples(format)?;
-    let mut rg = ReplayGain::new(spec.rate as usize)
-        .ok_or_else(|| anyhow!("This sample rate is not supported: {}", spec.rate))?;
+    let mut rg = ReplayGain::new(spec.rate() as usize)
+        .ok_or_else(|| anyhow!("This sample rate is not supported: {}", spec.rate()))?;
     rg.process_samples(&samples);
     let (gain, peak) = rg.finish();
     Ok((gain, peak))
 }
 
-fn read_interleaved_samples(format: &mut Box<dyn FormatReader>) -> anyhow::Result<(SignalSpec, Vec<f32>)> {
-    let track = format.default_track().ok_or_else(|| anyhow!("Can't get default track"))?;
+fn read_interleaved_samples(format: &mut Box<dyn FormatReader>) -> anyhow::Result<(AudioSpec, Vec<f32>)> {
+    let track = format.default_track(TrackType::Audio).ok_or_else(|| anyhow!("Can't get default track"))?;
     let track_id = track.id;
-    let codec = symphonia::default::get_codecs();
-    let mut decoder = codec.make(&track.codec_params, &DecoderOptions::default())
+    let codec_params = if let Some(codec) = &track.codec_params &&
+        let Some(audio_codec) = codec.audio() {
+        audio_codec
+    } else {
+        Err(ParseError::FormatUnsupported)?
+    };
+    let mut decoder = symphonia::default::get_codecs()
+        .make_audio_decoder(&codec_params, &AudioDecoderOptions::default())
         .with_context(|| "Can't create decoder")?;
 
-    let mut _sample_count = 0;
-    let mut sample_buf = None;
     let mut spec = None;
+    let mut samples: Vec<f32> = Vec::new();
+    let mut samples_buf: Vec<f32> = Vec::new();
+    let mut _total_sample_count = 0;
 
-    let mut samples: Vec<f32> =  Vec::new();
-    loop {
-        // Get the next packet from the format reader.
-        let packet = match format.next_packet() {
-            Ok(x) => { x }
-            Err(_err) => {
-                break
-            }
-        };
-
+    while let Ok(Some(packet)) = format.next_packet() {
         // If the packet does not belong to the selected track, skip it.
-        if packet.track_id() != track_id {
+        if packet.track_id != track_id {
             continue;
         }
 
         // Decode the packet into audio samples, ignoring any decode errors.
+        // Copy all the samples into a vector in the f32 sample format in channel interleaved order.
         match decoder.decode(&packet) {
             Ok(audio_buf) => {
-                // The decoded audio samples may now be accessed via the audio buffer if per-channel
-                // slices of samples in their native decoded format is desired. Use-cases where
-                // the samples need to be accessed in an interleaved order or converted into
-                // another sample format, or a byte buffer is required, are covered by copying the
-                // audio buffer into a sample buffer or raw sample buffer, respectively. In the
-                // example below, we will copy the audio buffer into a sample buffer in an
-                // interleaved order while also converting to a f32 sample format.
-
                 // If this is the *first* decoded packet, create a sample buffer matching the
                 // decoded audio buffer format.
-                if sample_buf.is_none() {
+                if spec.is_none() {
                     // Get the audio buffer specification.
-                    spec = Some(*audio_buf.spec());
-
-                    // Get the capacity of the decoded buffer. Note: This is capacity, not length!
-                    let duration = audio_buf.capacity() as u64;
-
-                    // Create the f32 sample buffer.
-                    sample_buf = Some(SampleBuffer::<f32>::new(duration, spec.unwrap()));
+                    spec = Some(audio_buf.spec().to_owned());
                 }
 
-                // Copy the decoded audio buffer into the sample buffer in an interleaved format.
-                if let Some(buf) = &mut sample_buf {
-                    buf.copy_interleaved_ref(audio_buf);
-                    // Append buf.samples to `samples``
-                    samples.extend(buf.samples());
-                    // The samples may now be access via the `samples()` function.
-                    _sample_count += buf.samples().len();
-                }
+                // Ensure the vector is large enough to hold all the samples.
+                samples_buf.resize(audio_buf.samples_interleaved(), f32::MID);
+                // Copy the audio samples from the generic audio buffer to the vector in interleaved
+                // order. The sample format to convert to is inferred from the type of the Vec.
+                audio_buf.copy_to_slice_interleaved(&mut samples_buf);
+
+                _total_sample_count += samples_buf.len();
+                samples.extend(&samples_buf);
             }
             Err(SymphoniaError::DecodeError(_)) => (),
             Err(_) => break,
